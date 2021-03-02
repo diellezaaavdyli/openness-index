8cc51cadbab3604d35cb28524a696c077defddf6
GentianV <gentian@opendatakosovo.org>
2021-03-02 20:41:28 +0100

    FrontPage-Login
    Go to login page as a first opened page in project.

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@
 */
 
 Route::group(['middleware' => ['get.menu']], function () {
-    Route::get('/', function () {           return view('dashboard.homepage'); });
+    Route::get('/', function () {           return view('auth.login'); });
+    Route::get('/homepage', function () {           return view('dashboard.homepage'); });
+
 
     Route::group(['middleware' => ['role:user']], function () {
         Route::get('/colors', function () {     return view('dashboard.colors'); });
