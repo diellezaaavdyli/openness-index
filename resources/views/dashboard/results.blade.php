a09abd976149a75ff1e651a3be8605c8f6483076
GentianV <gentian@opendatakosovo.org>
2021-03-05 03:36:58 +0100

    Results-final

--- resources/views/dashboard/results.blade.php
+++ resources/views/dashboard/results.blade.php
@@ -1,77 +1,70 @@
 @extends('dashboard.base')
 
 @section('content')
+<div class="container-fluid">
+  <div class="fade-in">
 
-          <div class="container-fluid">
-            <div class="fade-in">
-              <div class="card-columns cols-2">
-                <div class="card">
-                  <div class="card-header">Line Chart
-                    <div class="card-header-actions"><a class="card-header-action" href="http://www.chartjs.org" target="_blank"><small class="text-muted">docs</small></a></div>
-                  </div>
-                  <div class="card-body">
-                    <div class="c-chart-wrapper">
-                      <canvas id="canvas-1"></canvas>
-                    </div>
-                  </div>
-                </div>
-                <div class="card">
-                  <div class="card-header">Bar Chart
-                    <div class="card-header-actions"><a class="card-header-action" href="http://www.chartjs.org" target="_blank"><small class="text-muted">docs</small></a></div>
-                  </div>
-                  <div class="card-body">
-                    <div class="c-chart-wrapper">
-                      <canvas id="canvas-2"></canvas>
-                    </div>
-                  </div>
-                </div>
-                <div class="card">
-                  <div class="card-header">Doughnut Chart
-                    <div class="card-header-actions"><a class="card-header-action" href="http://www.chartjs.org" target="_blank"><small class="text-muted">docs</small></a></div>
-                  </div>
-                  <div class="card-body">
-                    <div class="c-chart-wrapper">
-                      <canvas id="canvas-3"></canvas>
-                    </div>
-                  </div>
-                </div>
-                <div class="card">
-                  <div class="card-header">Radar Chart
-                    <div class="card-header-actions"><a class="card-header-action" href="http://www.chartjs.org" target="_blank"><small class="text-muted">docs</small></a></div>
-                  </div>
-                  <div class="card-body">
-                    <div class="c-chart-wrapper">
-                      <canvas id="canvas-4"></canvas>
-                    </div>
-                  </div>
-                </div>
-                <div class="card">
-                  <div class="card-header">Pie Chart
-                    <div class="card-header-actions"><a class="card-header-action" href="http://www.chartjs.org" target="_blank"><small class="text-muted">docs</small></a></div>
-                  </div>
-                  <div class="card-body">
-                    <div class="c-chart-wrapper">
-                      <canvas id="canvas-5"></canvas>
-                    </div>
-                  </div>
-                </div>
-                <div class="card">
-                  <div class="card-header">Polar Area Chart
-                    <div class="card-header-actions"><a class="card-header-action" href="http://www.chartjs.org" target="_blank"><small class="text-muted">docs</small></a></div>
-                  </div>
-                  <div class="card-body">
-                    <div class="c-chart-wrapper">
-                      <canvas id="canvas-6"></canvas>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+<div class="card text-center results-card">
+  <div class="card-header results-card1">
+    <strong>KOSOVO</strong>
+  </div>
+  <div class="card-body results-card2">
+    <h5 class="card-title">Open data score of Kosovo is: 34.27 %</h5>
+    <p class="card-text">Between 46 institutions monitored, 
+      Ministry of Local Government Administration got the best score of 75.24 %, 
+      while Municipality of Leposaviq got the worst score of 0.00 %</p>
+  </div>
+  <div class="card-footer text-muted results-card3">
+    <strong>2021</strong>
+  </div>
+</div>
+<br><hr><br>
+
+    <div id="chartContainer" style="height: 370px; width: 75%; margin:auto;"></div>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+  </div>
+</div>
+<br>
 
 @endsection
 
 @section('javascript')
+
+<script>
+  window.onload = function () {
+  
+  var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    theme: "light3", // "light1", "light2", "dark1", "dark2"
+    title: {
+      text: "Open data score by countries"
+    },
+    axisY: {
+      title: "Score (in %)",
+      suffix: "%"
+    },
+    axisX: {
+      title: "Countries"
+    },
+    data: [{
+      type: "column",
+      yValueFormatString: "#,##0.0#\"%\"",
+      dataPoints: [
+        { label: "Albania", y: 59.1 },	
+        { label: "Bosnia and Herzegovina", y: 54.70 },	
+        { label: "Kosovo", y: 63.40 },
+        { label: "North Macedonia", y: 49.50 },	
+        { label: "Montenegro", y: 86.30 },
+        { label: "Serbia", y: 57.80 }
+        
+      ]
+    }]
+  });
+  chart.render();
+  }
+  </script>
+
     <script src="{{ asset('js/Chart.min.js') }}"></script>
     <script src="{{ asset('js/coreui-chartjs.bundle.js') }}"></script>
     <script src="{{ asset('js/charts.js') }}"></script>
