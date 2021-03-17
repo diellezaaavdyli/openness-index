64d90c4c3957c735879e933b3ef4bd2da7b9f95f
GentianV <gentian@opendatakosovo.org>
2021-03-17 11:32:40 +0100

    dashboard-changes

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Institutions</div>
+                <div class="card-header">Forms</div>
 
                 <div class="card-body">
                     @if (session('status'))
